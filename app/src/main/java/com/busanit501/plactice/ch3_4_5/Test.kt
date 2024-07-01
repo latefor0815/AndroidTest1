6ccab8054c95a9d44a89eb5996f45b7b978f32a6
LeeHyunJong <vvlddiz@gmail.com>
2024-07-01 17:42:36 +0900

    7월1일 마무리

--- app/src/main/java/com/busanit501/plactice/ch3_4_5/Test.kt
+++ app/src/main/java/com/busanit501/plactice/ch3_4_5/Test.kt
@@ -32,6 +32,41 @@ fun main() { // 선언만 해도 가능. 초기화를 조금 늦게 해도 됨.
     println("by lazy 테스트")
     println(data)
 
+    //배열 만들기
+    val data3 : Array<String> = Array(3,{""})
+    data3[0] = "lsy1"
+    data3[1] = "lsy2"
+    data3.set(2,"lsy3")
+    //가져오기 get
+    println("data3 배열 가져오기 테스트 : 사이즈 : ${data3.size}, data[0] : ${data3[0]}"
+    + ", data3.get(1) : ${data3.get(1)}")
 
+
+    val arrayData2 = intArrayOf(1,2,3)
+    val arrayData3 = booleanArrayOf(true, false, true)
+    val arrayData4 = arrayOf<String>("국수", "국밥", "된장찌개")
+    val arrayData5 = arrayOf<Int>(1,2,3)
+
+    var list1 = listOf<Int>(1,2,3)
+    var list2 = listOf<String>("1","2","3")
+
+    var mutableList1 = mutableListOf<Int>(1,2,3)
+    var mutableList2 = mutableListOf<String>("1","2","3")
+    mutableList2.add(4,"4")
+
+    //Map , 고정길이
+    var map1 = mapOf<String,String>(Pair("1","one"), Pair("2","two"), "3" to "three")
+
+    var mutableMap1 = mutableMapOf<String,String>()
+
+    //when
+    var data = 1
+    when(data) {
+        1 -> println("1번 국수")
+        2 -> println("2번 국밥")
+        else -> {
+            println("기본 된장찌깨")
+        }
+    }
 }
 //변경 테스트23
